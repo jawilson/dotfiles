3920cf5c1c51f93748a4e3b3bd727d036293a74f
Jeffery Wilson <jeff.wilson@blinemedical.com>
2016-08-01 11:05:20 -0400

    zsh: update work stuff

--- oh-my-zsh/dotfiles-custom/work-shortcuts.zsh
+++ oh-my-zsh/dotfiles-custom/work-shortcuts.zsh
@@ -1,6 +1,10 @@
 home=/cygdrive/c/Users/jeff.wilson
 C=/cygdrive/c
 
+cn=/cygdrive/c/Projects/CaptureNode
+capturenode=/cygdrive/c/Projects/CaptureNode/capture-node
+recorder=/cygdrive/c/Projects/CaptureNode/video-recorder-windows
+
 cube=/cygdrive/c/Projects/Cube/trunk
 cubelog=/cygdrive/c/Projects/Cube/logs
 src=/cygdrive/c/Users/jeff.wilson/src
