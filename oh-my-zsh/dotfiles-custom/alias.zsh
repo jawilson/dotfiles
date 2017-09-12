14044fe899e87e873517efdd7de3971ee021be38
Jeffery Wilson <jeff@jeffalwilson.com>
2017-09-11 22:55:20 -0400

    zsh: add alias for update PlexPy

--- oh-my-zsh/dotfiles-custom/alias.zsh
+++ oh-my-zsh/dotfiles-custom/alias.zsh
@@ -28,3 +28,11 @@ alias dedup="awk '!seen[$0]++'"
 
 # plex
 alias plexup="sudo sh -c \"rm -rf /tmp/pms && install -d /tmp/pms && wget --content-disposition https://plex.tv/downloads/latest/1\?channel\=8\&build\=linux-ubuntu-x86_64\&distro\=ubuntu\&X-Plex-Token\=ZYD6a4uHqssyqpUnKy8d --directory-prefix=/tmp/pms/ && find '/tmp/pms/' -type f | head -1 | xargs -I{} dpkg -i '{}' && rm -rf /tmp/pms\""
+alias plexpyup="docker pull linuxserver/plexpy && if docker stop plexpy >/dev/null 2>&1; then  docker rm plexpy >/dev/null; fi && \
+docker run \
+  --name=plexpy -v /media/raid/plexmediaserver/plexpy:/config --restart always \
+  -v \"/media/raid/plexmediaserver/Library/Application Support/Plex Media Server/Logs\":/logs:ro \
+  -e PGID=115 -e PUID=110 \
+  -e TZ=America/New_York \
+  -p 8181:8181 \
+  -d linuxserver/plexpy"
