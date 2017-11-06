056790d261e2de9c5b95a33bfb8efb3a09729e80
Jeffery Wilson <jeff@jeffalwilson.com>
2017-11-06 10:59:56 -0500

    Update work setup for Ubuntu on Windows

--- oh-my-zsh/dotfiles-custom/work-shortcuts.zsh
+++ oh-my-zsh/dotfiles-custom/work-shortcuts.zsh
@@ -1,10 +1,13 @@
-home=/cygdrive/c/Users/jeff.wilson
-C=/cygdrive/c
+home=/mnt/c/Users/jeff.wilson
+C=/mnt/c
 
-cn=/cygdrive/c/Projects/CaptureNode
-capturenode=/cygdrive/c/Projects/CaptureNode/capture-node
-recorder=/cygdrive/c/Projects/CaptureNode/video-recorder-windows
+vc=/mnt/c/Projects/video-conductor
 
-cube=/cygdrive/c/Projects/Cube/trunk
-cubelog=/cygdrive/c/Projects/Cube/logs
-src=/cygdrive/c/Users/jeff.wilson/src
+cn=/mnt/c/Projects/CaptureNode
+capturenode=/mnt/c/Projects/CaptureNode/capture-node
+recorder=/mnt/c/Projects/CaptureNode/video-recorder-windows
+simmerge=/mnt/c/Projects/CaptureNode/simmerge
+
+cube=/mnt/c/Projects/Cube/trunk
+cubelog=/mnt/c/Projects/Cube/logs
+src=/mnt/c/Users/jeff.wilson/src
