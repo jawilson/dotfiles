33741f62496db3d9d2c9a8d3dd7a627dc4a282c2
Jeffery Wilson <jeffery.wilson@laerdal.com>
2023-10-10 13:29:24 -0400

    Improve GStreamer environment

--- oh-my-zsh/dotfiles-custom/work.zsh
+++ oh-my-zsh/dotfiles-custom/work.zsh
@@ -2,9 +2,11 @@ if [ "$(uname -s)" = "Linux" ]; then
     export MEDIASOUP_ANNOUNCED_IP=$(hostname -I|cut -d' ' -f1)
 fi
 
-if [ -d /opt/gstreamer ]; then
-    export PATH=/opt/gstreamer/bin:$PATH
-    export PKG_CONFIG_PATH=/opt/gstreamer/lib/x86_64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH
+GSTREAMER_ROOT=/opt/gstreamer
+if [ -d $GSTREAMER_ROOT ]; then
+    export PATH=$GSTREAMER_ROOT/bin:$PATH
+    export PKG_CONFIG_PATH=$GSTREAMER_ROOT/lib/x86_64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH
+    export C_INCLUDE_PATH=$GSTREAMER_ROOT/include:$GSTREAMER_ROOT/include/gstreamer-1.0:{C_INCLUDE_PATH:+:$C_INCLUDE_PATH}
 fi
 
 gst_build_dir="$HOME/src/gst-build"
