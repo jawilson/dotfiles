8968a2c222fa47e52b01ced09962a380d8b49315
Jeffery Wilson <jeffery.wilson@laerdal.com>
2023-11-21 08:40:44 -0500

    Fix adding GStreamer configs

--- oh-my-zsh/dotfiles-custom/work.zsh
+++ oh-my-zsh/dotfiles-custom/work.zsh
@@ -5,8 +5,9 @@ fi
 GSTREAMER_ROOT=/opt/gstreamer
 if [ -d $GSTREAMER_ROOT ]; then
     export PATH=$GSTREAMER_ROOT/bin:$PATH
-    export PKG_CONFIG_PATH=$GSTREAMER_ROOT/lib/x86_64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH
-    export C_INCLUDE_PATH=$GSTREAMER_ROOT/include:$GSTREAMER_ROOT/include/gstreamer-1.0:{C_INCLUDE_PATH:+:$C_INCLUDE_PATH}
+    export PKG_CONFIG_PATH=$GSTREAMER_ROOT/lib/x86_64-linux-gnu/pkgconfig${PKG_CONFIG_PATH:+:$PKG_CONFIG_PATH}
+    export C_INCLUDE_PATH=$GSTREAMER_ROOT/include:$GSTREAMER_ROOT/include/gstreamer-1.0${C_INCLUDE_PATH:+:$C_INCLUDE_PATH}
+    export CPLUS_INCLUDE_PATH=$GSTREAMER_ROOT/include:$GSTREAMER_ROOT/include/gstreamer-1.0${CPLUS_INCLUDE_PATH:+:$CPLUS_INCLUDE_PATH}
 fi
 
 gst_build_dir="$HOME/src/gst-build"
