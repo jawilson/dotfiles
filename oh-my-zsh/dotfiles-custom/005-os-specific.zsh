9835dc3025afb6a9b529f93fd869778fa9d2d828
Jeffery Wilson <jeff@jeffalwilson.com>
2024-04-12 16:58:39 -0400

    Ensure unix line endings for get-cred-password

--- oh-my-zsh/dotfiles-custom/005-os-specific.zsh
+++ oh-my-zsh/dotfiles-custom/005-os-specific.zsh
@@ -1,6 +1,6 @@
 if [[ "$OSTYPE" == "msys" || -e "/proc/sys/fs/binfmt_misc/WSLInterop" ]]; then
   if powershell.exe -Command "if(-Not (Get-Command Get-StoredCredential -errorAction SilentlyContinue)) { exit 1; }" &> /dev/null; then
-    alias get-cred-password='f () { powershell.exe -Command "\$BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR((Get-StoredCredential -Target LegacyGeneric:target=$1).Password); [System.Runtime.InteropServices.Marshal]::PtrToStringAuto(\$BSTR); [Runtime.InteropServices.Marshal]::ZeroFreeBSTR(\$BSTR)" | tr -d "[:space:]" }; f'
+    alias get-cred-password='f () { powershell.exe -Command "\$BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR((Get-StoredCredential -Target LegacyGeneric:target=$1).Password); [System.Runtime.InteropServices.Marshal]::PtrToStringAuto(\$BSTR); [Runtime.InteropServices.Marshal]::ZeroFreeBSTR(\$BSTR)" | dos2unix  }; f'
   else
     alias get-cred-password='echo "Get-StoredCredential not found, run 'Install-Module CredentialManager' in an elevated PowerShell session to install it"'
   fi
