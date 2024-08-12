b00686d1386f7b192d74131fa4dad611cf396cf2
Jeffery Wilson <jeff@jeffalwilson.com>
2024-08-12 18:20:08 -0400

    update ghcr login

--- oh-my-zsh/dotfiles-custom/alias.zsh
+++ oh-my-zsh/dotfiles-custom/alias.zsh
@@ -31,6 +31,6 @@ docker run \
 
 # docker
 alias docker-compose="docker compose"
-alias dlggh="get-cred-password ghcr.io | docker login ghcr.io -u jawilson --password-stdin"
+alias dlggh="get-cred-password docker:ghcr.io | docker login ghcr.io -u PAT --password-stdin"
 alias dlgaws="aws ecr get-login-password | docker login --username AWS --password-stdin \$(aws sts get-caller-identity --query Account --output text).dkr.ecr.\$(aws configure get region).amazonaws.com"
 alias dlga="dlgaws; dlggh"
