4bc01d6218e53bf49c8e75f0265452a1f3034239
Jeffery Wilson <jeff@jeffalwilson.com>
2026-05-27 12:44:10 -0400

    add general vsclean alias

--- oh-my-zsh/dotfiles-custom/work-alias.zsh
+++ oh-my-zsh/dotfiles-custom/work-alias.zsh
@@ -1,11 +1,13 @@
 alias cubelocale='cd src/Core/Cube.MasterService.Web/locale && for l in en_US es_ES fr_FR ja_JA ko_KR pt_PT ru_RU zh_CHS; do install -d ${l} ${l}_LC ${l}_Custom; done; cd -'
 
-alias cnclean='cd ~cn; git clean -xdf \
+alias vsclean='git clean -xdf \
+  -e "*.env" \
   -e "*.user" \
   -e ".vs" \
   -e "*.*proj" \
   -e "*.cs" \
   -e "*.c" \
   -e "*.cpp" \
-  -e "*.h" \
-  src/; cd - > /dev/null'
+  -e "*.h"'
+
+alias cnclean='cd ~cn; vsclean src/; cd - > /dev/null'
